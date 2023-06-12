9d12260a9f724930b23bac327c3bf336f262aa51
alexia-pratensi <alexiapratensi@hotmail.fr>
2023-06-12 22:55:12 +0200

    initial commit

---
+++ src/test/java/fr/alexia/backendapi/BackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.alexia.backendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
