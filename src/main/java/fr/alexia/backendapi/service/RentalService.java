05288d64603a0a211a4132660d208edadd111800
alexia-pratensi <alexiapratensi@hotmail.fr>
2023-06-13 19:16:34 +0200

    added rental service

---
+++ src/main/java/fr/alexia/backendapi/service/RentalService.java
@@ -0,0 +1,33 @@
+package fr.alexia.backendapi.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.alexia.backendapi.model.Rental;
+import fr.alexia.backendapi.repository.RentalRepository;
+
+@Service
+public class RentalService {
+ @Autowired
+    private RentalRepository rentalRepository;
+
+    public Optional<Rental> getRental(final Long id) {
+        return rentalRepository.findById(id);
+    }
+
+    public Iterable<Rental> getRentals() {
+        return rentalRepository.findAll();
+    }
+
+    public void deleteRental(final Long id) {
+        rentalRepository.deleteById(id);
+    }
+
+    public Rental saveRental(Rental rental) {
+        Rental savedRental = rentalRepository.save(rental);
+        return savedRental;
+    }
+
+}
